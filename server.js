31f594f847fba87d0a840c084a9fcf31bb588e54
younguk86@gmail.com <younguk86@gmail.com>
2022-05-21 15:02:27 +0900

    refresh architecture and prisma model

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
-
-const client = new PrismaClient()
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-    Query:{
-        hello: () => "Wolrd",
-    }
-}
+import schema from "./schema"
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  schema,
 });
 
 server.listen().then(() => console.log("Server is running on http://localhost:4000/graphql 🚀"));
