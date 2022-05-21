703e3e3bb8c1f715f75d18aabca5d1dc1b08bedf
younguk86@gmail.com <younguk86@gmail.com>
2022-05-21 13:06:01 +0900

    .

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient()
+
 const typeDefs = gql`
   type Query {
     hello: String
